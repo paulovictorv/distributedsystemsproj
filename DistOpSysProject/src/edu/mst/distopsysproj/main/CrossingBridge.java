75f89e848aabdedfe25288e0808a5a79e86550a3
Julio <Julio@Julio-VAIO>
2013-04-18 15:36:26 -0500

    Start using JADE framework

--- DistOpSysProject/src/edu/mst/distopsysproj/main/CrossingBridge.java
+++ DistOpSysProject/src/edu/mst/distopsysproj/main/CrossingBridge.java
@@ -1,9 +1,25 @@
 package edu.mst.distopsysproj.main;
 
-public class CrossingBridge {
+import jade.core.AID;
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
 
-	public static void main(String[] args) {
-		System.out.println("Olá GIT");
+public class CrossingBridge extends Agent {
+
+	@Override
+	protected void setup() {
+		System.out.println("Hello! " + getAID().getName() + " is ready!");
+		
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.addReceiver(new AID("agenttwo", AID.ISLOCALNAME));
+		msg.setLanguage("English");
+		msg.setContent("Testing message passing");
+		send(msg);
+		System.out.println(getAID().getName() + " sent message");
 	}
-
+	
+	@Override
+	protected void takeDown() {
+		System.out.println("Bye! " + getAID().getName() + " is shutting down!");
+	}
 }
