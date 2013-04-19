be6a43bb825c2959be27509dda370f52abc58480
paulovictorv <paulovictorv@gmail.com>
2013-04-19 15:21:31 -0500

    adding array

--- DistOpSysProject/src/edu/mst/distopsysproj/main/Person.java
+++ DistOpSysProject/src/edu/mst/distopsysproj/main/Person.java
@@ -5,6 +5,6 @@ import jade.core.Agent;
 public class Person extends Agent {
 	private static final long serialVersionUID = 1L;
 
-	
+	private static String[] persons = {"person1", "person2", "person3", "person4"};
 	
 }
