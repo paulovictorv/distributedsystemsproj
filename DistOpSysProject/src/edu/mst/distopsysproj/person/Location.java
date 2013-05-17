d55b526c81c304eb2d9f70984c67effa26ef1ed8
Julio <Julio@Julio-VAIO>
2013-05-16 20:15:56 -0500

    Add documentation to edu.mst.distopsysproj.person classes

--- DistOpSysProject/src/edu/mst/distopsysproj/person/Location.java
+++ DistOpSysProject/src/edu/mst/distopsysproj/person/Location.java
@@ -1,8 +1,19 @@
 package edu.mst.distopsysproj.person;
 
+/**
+ *  Enum to define the location of an Agent in the environment
+ *  either side A, side B or crossing the bridge.
+ * 
+ * @author Julio Zynger, Paulo Victor Melo
+ */
 public enum Location {
 	A, B, BRIDGE;
 
+	/**
+	 * Return the opposite location's enum regarding to the bridge
+	 * so, if the parameter <code>lastLocation</code> is A, the return
+	 * will be B, and so on.
+	 */
 	public static Location getOppositeLocation(Location lastLocation) {
 		if(lastLocation == A) return B;
 		else if(lastLocation == B) return A;
--- DistOpSysProject/src/edu/mst/distopsysproj/person/Person.java
+++ DistOpSysProject/src/edu/mst/distopsysproj/person/Person.java
@@ -10,14 +10,43 @@ import java.util.HashMap;
 import edu.mst.distopsysproj.util.Bridge;
 import edu.mst.distopsysproj.util.ProtocolConstants;
 
+/**
+ * Class <code>Person</code> that extends from JADE's <code>Agent</code>
+ * will represent each participant in the Ricart-Agrawala's protocol
+ * implementation, being allowed to cross the 'bridge', that represents
+ * the critical-section following the rules of mutual exclusion.<br /><br />
+ * 
+ * A parameter sent as argument in command-line will define the type of
+ * protocol, if <code>false</code>, only one Person will be able to cross
+ * the bridge at each time. If <code>true</code>, two persons will be able
+ * to cross the bridge at each time, but only if they came from the same side
+ * of the river.
+ * 
+ * @author Julio Zynger, Paulo Victor Melo
+ */
 public class Person extends Agent {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Array of ids of each <code>Person</code> participating in
+	 * the mechanism's execution.
+	 */
 	public static String[] persons = { "person1", "person2", "person3", "person4" };
 
+	/**
+	 * Single instance of the bridge, accessible at any time in the computation
+	 */
 	private Bridge bridge;
 	
+	/**
+	 * Variable to store the <code>lastLocation</code> of this <code>Person</code>,
+	 * so the entering in the critical-section can be defined in the protocol with
+	 * multiple-visitors in the bridge at once.
+	 */
 	private Location lastLocation;
+	/**
+	 * Current {@link Location} of this {@link Person}.
+	 */
 	private Location location;
 
 	private Boolean tryCS, want, in;
@@ -38,6 +67,10 @@ public class Person extends Agent {
 		
 		bridge = Bridge.getInstance();
 
+		/**
+		 * Set the initial position of each {@link Person} in the
+		 * GUI's canvas.
+		 */
 		if(getName().contains("person1")
 				|| getName().contains("person2")){
 			this.location = Location.A;
@@ -48,17 +81,41 @@ public class Person extends Agent {
 			this.lastLocation = Location.B;
 		}
 		
+		/**
+		 * Get the arguments passed to the agents that determine
+		 * what type of protocol to use. Reading <code>true</code>
+		 * will allow two people in the bridge at once, and only
+		 * one otherwise.
+		 */
 		Object[] args = getArguments();
 		boolean twoVisitors = Boolean.valueOf(args[0].toString()).booleanValue();
 		
 		addBehaviour(new CrossBridgeBehaviourAgrawala(twoVisitors));
 	}
 
+	/**
+	 * {@link CyclicBehaviour} that will implement the
+	 * Ricart-Agrawala's protocol in this application.
+	 * The behavior will treat the message passing between the
+	 * {@link Person}s and the communication between them and
+	 * the {@link Bridge}.
+	 * 
+	 * @author Julio Zynger, Paulo Victor Melo
+	 *
+	 */
 	private class CrossBridgeBehaviourAgrawala extends CyclicBehaviour {
 		private static final long serialVersionUID = 1L;
+		/**
+		 * Constant that defines the time the thread will be frozen
+		 * when entering and leaving the critical-section. Commonly
+		 * used only for testing purposes.
+		 */
 		private static final long BRIDGE_WAITING_TIME = 1500;
 
-		// true if two visitors can be at the bridge at once, false otherwise
+		/**
+		 * <code>true</code> if two visitors can be at the bridge at once,
+		 * <code>false</code> otherwise
+		 */
 		private boolean twoVisitors;
 		
 		public CrossBridgeBehaviourAgrawala(boolean twoVisitors) {
@@ -68,14 +125,19 @@ public class Person extends Agent {
 		@Override
 		public void action() {
 			if(tryCS){
+				// process request to enter the critical-section
+				// sending messages to every other process involved
+				// in the system.
 				ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
 				request.setContent(ProtocolConstants.MSGTYPE_REQUEST);
 				request.addUserDefinedParameter(
 						ProtocolConstants.TWOVISITORSPROTOCOL_LASTLOCATION,
 						lastLocation.toString());
 				timestamp = System.currentTimeMillis();
-				request.addUserDefinedParameter(ProtocolConstants.TIMESTAMP, String.valueOf(timestamp));
+				request.addUserDefinedParameter(ProtocolConstants.TIMESTAMP,
+						String.valueOf(timestamp));
 				for (String target : persons){
+					// will only send messages to agents different than himself
 					if(!getName().contains(target)){							
 						request.addReceiver(new AID(target, AID.ISLOCALNAME));
 					}
@@ -86,12 +148,14 @@ public class Person extends Agent {
 			}
 	
 			if(acksNumber == persons.length-1){
-				in = true;
 				// process enters CS
-				//System.out.println("Person " + myAgent.getLocalName() + " is on the bridge");
+				in = true;
+				// asks the bridge to add himself to the queue
 				bridge.enterBridge(myAgent.getLocalName());
+				// sets the new location variables
 				lastLocation = location;
 				location = Location.BRIDGE;
+				
 				want = false;
 				try {
 					Thread.sleep(BRIDGE_WAITING_TIME);
@@ -101,16 +165,18 @@ public class Person extends Agent {
 			}
 			
 			if(in && !want){
+				// process left CS - reset variables and locations
 				in = false;
 				acksNumber = 0;
-				// process left CS
-				//System.out.println("Person " + myAgent.getLocalName() + " left the bridge");
 				location = Location.getOppositeLocation(lastLocation);
 				bridge.leftBridge(myAgent.getLocalName(), location);
 				
+				// send ack to the buffered request to keep
+				// the protocol going
 				ACLMessage ack = new ACLMessage(ACLMessage.REQUEST);
 				ack.setContent(ProtocolConstants.MSGTYPE_ACK);
-				ack.addUserDefinedParameter(ProtocolConstants.TIMESTAMP, String.valueOf(System.currentTimeMillis()));
+				ack.addUserDefinedParameter(ProtocolConstants.TIMESTAMP,
+						String.valueOf(System.currentTimeMillis()));
 				for (String p : acksMap.keySet()) {
 					Boolean val = acksMap.get(p);
 					if(val){
@@ -120,6 +186,7 @@ public class Person extends Agent {
 					}
 				}
 				send(ack);
+				
 				tryCS = true;
 				try {
 					Thread.sleep(BRIDGE_WAITING_TIME);
@@ -133,6 +200,9 @@ public class Person extends Agent {
 				String content = msg.getContent();
 				
 				if(content.equals(ProtocolConstants.MSGTYPE_REQUEST)) {
+					// if a request was received, this agent will need
+					// to check the priority of the processes to define
+					// who is going to enter the CS
 					String locString = msg.getUserDefinedParameter(
 							ProtocolConstants.TWOVISITORSPROTOCOL_LASTLOCATION);
 					Location senderLastLocation = Location.valueOf(locString);
@@ -140,21 +210,34 @@ public class Person extends Agent {
 					if(!want || compareTimestamps(msg, timestamp)){
 						sendAck(msg);
 					}else{
+						// if two visitors are allowed on the bridge (depending
+						// on the chosen protocol), the last location of the requester
+						// must be the checked to be the same as this agents',
+						// otherwise they are not allowed to enter the bridge
 						if(twoVisitors && senderLastLocation == lastLocation){
 							sendAck(msg);
 						}else{
+							// if not allowed to enter the bridge, buffer the request
 							String sender = msg.getSender().getLocalName();
 							acksMap.put(sender, true);
 						}
 					}
 				}else if(content.equals(ProtocolConstants.MSGTYPE_ACK)) {
+					// if a message received was an ACK, increment the counter
+					// to perform a later check when trying to enter the CS
 					acksNumber = acksNumber + 1;
 				}else{
+					// if the message was neither a request nor an ACK,
+					// put it back on the buffer so it can be read later
 					myAgent.putBack(msg);
 				}
 			}
 		}
 		
+		/**
+		 * Build an ACK reply to a given {@link ACLMessage} and send it
+		 * @param msg Message to reply to
+		 */
 		private void sendAck(ACLMessage msg) {
 			ACLMessage ack = msg.createReply();
 			ack.setContent(ProtocolConstants.MSGTYPE_ACK);
@@ -166,12 +249,24 @@ public class Person extends Agent {
 			send(ack);
 		}
 		
+		/**
+		 * Check to see which is the priority process to enter the CS
+		 * based on their timestamp.
+		 * 
+		 * @param msg Message whose timestamp will be checked
+		 * @param localTimestamp Timestamp of the local process
+		 * @return <code>true</code> if this {@link Person}'s priority is
+		 * higher, or <code>false</code> otherwise.
+		 */
 		private boolean compareTimestamps(ACLMessage msg, long localTimestamp){
-			long msgTimestamp = Long.valueOf(msg.getUserDefinedParameter(ProtocolConstants.TIMESTAMP)).longValue();
+			long msgTimestamp = Long.valueOf(
+					msg.getUserDefinedParameter(ProtocolConstants.TIMESTAMP)
+					).longValue();
 			if ( msgTimestamp < localTimestamp ){
 				return true;
 			} else if( msgTimestamp == localTimestamp ){
-				return getLocalName().compareTo(msg.getSender().getLocalName()) > 0;
+				return getLocalName().compareTo(
+						msg.getSender().getLocalName()) > 0;
 			} else {
 				return false;
 			}
